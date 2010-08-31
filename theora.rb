f7ec2037aa8e6fc862970d9e7ab1777293225f04
David Eisinger <david.eisinger@viget.com>
2010-08-30 22:54:41 -0400

    Occasionally ask for full scale

--- theora.rb
+++ theora.rb
@@ -5,15 +5,21 @@ scales = JSON.parse(open("scales.json").read)
 
 loop do
   scale = scales.keys[rand(scales.size)]
-  position = rand(7)
 
-  print "#{scale}, position #{position + 1}: "
-
-  response = STDIN.gets
-
-  if response.strip == scales[scale][position]
-    puts "Right!"
+  if rand(5) == 0
+    puts "#{scale} scale!"
+    STDIN.gets
   else
-    puts "Wrong, #{scales[scale][position]}"
+    position = rand(7)
+
+    print "#{scale}, position #{position + 1}: "
+
+    response = STDIN.gets
+
+    if response.strip == scales[scale][position]
+      puts "Right!"
+    else
+      puts "Wrong, #{scales[scale][position]}"
+    end
   end
 end
