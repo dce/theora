5b2747fa332e0dadad1ecd8edb0c967e808cdfb0
David Eisinger <david.eisinger@viget.com>
2010-08-30 23:22:06 -0400

    Remove known scales from list

--- theora.rb
+++ theora.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'json'
 
 scales = JSON.parse(open("scales.json").read)
+new_scales = {}
 
 print "(s)cales, (n)otes, or (b)oth? "
 questions = STDIN.gets.strip
@@ -10,14 +11,29 @@ loop do
   scale = scales.keys[rand(scales.size)]
 
   if questions == "s" || (questions == "b" && rand(5) == 0)
+    flats = scales[scale].inject(0) {|sum, note| sum + (note =~ /\wb/ ? 1 : 0) }
+    sharps = scales[scale].inject(0) {|sum, note| sum + (note =~ /\w#/ ? 1 : 0) }
+
+    scale_string = scales[scale] * " "
+    scale_string = "#{scale_string} (#{flats} flat#{"s" if flats > 1})" if flats > 0
+    scale_string = "#{scale_string} (#{sharps} sharp#{"s" if sharps > 1})" if sharps > 0
+
     print "#{scale} scale!"
     STDIN.gets
-    puts scales[scale] * " "
+    puts scale_string
+    print "Did you get it? (y/n) "
+    response = STDIN.gets.strip
+
+    if response == "y"
+      new_scales[scale] = scales.delete(scale)
+      if scales.empty?
+        scales = new_scales
+        new_scales = {}
+      end
+    end
   else
     position = rand(7)
-
     print "#{scale}, position #{position + 1}: "
-
     response = STDIN.gets.strip
 
     if response == scales[scale][position]
