3ff23d3a531b11f18e7e9959e9809922f5c043bd
David Eisinger <david.eisinger@viget.com>
2010-08-30 23:01:32 -0400

    Choose question type

--- theora.rb
+++ theora.rb
@@ -3,20 +3,24 @@ require 'json'
 
 scales = JSON.parse(open("scales.json").read)
 
+print "(s)cales, (n)otes, or (b)oth? "
+questions = STDIN.gets.strip
+
 loop do
   scale = scales.keys[rand(scales.size)]
 
-  if rand(5) == 0
-    puts "#{scale} scale!"
+  if questions == "s" || (questions == "b" && rand(5) == 0)
+    print "#{scale} scale!"
     STDIN.gets
+    puts scales[scale] * " "
   else
     position = rand(7)
 
     print "#{scale}, position #{position + 1}: "
 
-    response = STDIN.gets
+    response = STDIN.gets.strip
 
-    if response.strip == scales[scale][position]
+    if response == scales[scale][position]
       puts "Right!"
     else
       puts "Wrong, #{scales[scale][position]}"
